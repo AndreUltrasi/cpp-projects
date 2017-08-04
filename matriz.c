83ffe8e50ad1a72903dd8f57c2a3001263e99cd2
André Augusto Nunes Vieira <andreultrasi@gmail.com>
2017-08-03 23:19:40 -0300

    Delete matriz.c

--- matriz.c
+++
@@ -1,8 +0,0 @@
-include <stdio.h>
-include <stdlib.h>
-int main(){
-    int n1[3][4],n2[3][4];
-    for (int i = 0; i<2;i++){
-        for (int j = 0; j<3; j++){
-            printf("Índice [%d][%d]",i,j);
-            scanf("%d",&n1[i][j]);
